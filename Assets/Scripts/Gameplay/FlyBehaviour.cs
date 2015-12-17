87c72b03024170f5d9a3bc9a439018eb597abfca
Diego.Chiodini <chiodini.dev@gmail.com>
2015-12-17 17:09:10 +0000

    bee controller

---
+++ Assets/Scripts/Gameplay/FlyBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(DistanceJoint2D))]
+public class FlyBehaviour : MonoBehaviour
+{
+    Rigidbody2D _body;
+
+    Vector3 _direction = Vector3.zero;
+    float _actualDistance = 0f;
+    public float Distance = 1f;
+    public float RefreshRate = 1f;
+
+    public Transform target;
+    public float speed = 1f;
+
+    void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        DistanceJoint2D joint = GetComponent<DistanceJoint2D>();
+        joint.distance = Distance;
+        StartCoroutine(ChangeDirection());
+    }
+
+    private void Update()
+    {
+        _body.velocity =  _direction * speed * _actualDistance;
+    }
+
+    IEnumerator ChangeDirection()
+    {
+        yield return new WaitForSeconds(RefreshRate);
+
+        _direction = target.position - transform.position;
+        _direction = new Vector3((Random.value * 2) - 1, (Random.value * 2) - 1, (Random.value * 2) - 1);
+        _actualDistance = _direction.magnitude * Random.value;
+        _direction.Normalize();
+
+        StartCoroutine(ChangeDirection());
+    }
+}
