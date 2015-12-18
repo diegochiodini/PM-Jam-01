6e329bcb0a6d9062b20c92065f864c19ebbf8224
Diego.Chiodini <chiodini.dev@gmail.com>
2015-12-18 15:03:10 +0000

    ADD LAND TAG

--- Assets/Scripts/Constants/GameConstants.cs
+++ Assets/Scripts/Constants/GameConstants.cs
@@ -5,5 +5,6 @@ public class GameConstants
 {
     public const string TAG_POLLEN = "POLLEN";
     public const string TAG_PLAYER = "PLAYER";
+    public const string TAG_LAND = "LAND";
     public const string TAG_FLOWERSPAWNPOINT = "FLOWERSPAWNPOINT";
 }
--- Assets/Scripts/Gameplay/Pollen.cs
+++ Assets/Scripts/Gameplay/Pollen.cs
@@ -8,6 +8,11 @@ class Pollen : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-
+        Debug.Log("TRIGGER COLLISION!");
+        if (other.CompareTag(GameConstants.TAG_LAND))
+        {
+            Debug.Log("LAAAAAAAAAAAAAAAAAND!");
+            GetComponent<Collider2D>().isTrigger = false;
+        }
     }
 }
