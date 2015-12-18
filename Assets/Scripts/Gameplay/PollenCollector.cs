25db74360582c615c52b83e8f054a827748b054a
DA Sillitoe <kitto.art@gmail.com>
2015-12-18 14:37:25 +0000

    Adding beehives, game scoring and winning logic.

--- Assets/Scripts/Gameplay/PollenCollector.cs
+++ Assets/Scripts/Gameplay/PollenCollector.cs
@@ -1,26 +1,41 @@
 ﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
 using UnityEngine.Events;
 
-[RequireComponent(typeof(Collider2D))]
-[RequireComponent(typeof(Rigidbody2D))]
-public class PollenCollector : MonoBehaviour
+public class Player : MonoBehaviour 
 {
-    private Collider2D pollenCollider;
+    private Rigidbody2D rbody;
+    private CircleCollider2D collider;
+
+    private Vector2 defaultPosition;
+
+    public Collider2D pollenCollider;
+
     public UnityEvent onPollenDrop;
 
-    private void OnCollisionEnter2D(Collision2D other)
+    void Awake()
+    {
+        defaultPosition = transform.position;
+        rbody = GetComponent<Rigidbody2D>();
+        Assert.IsNotNull(rbody, "Player must have a Rigidbody2D");
+        collider = GetComponent<CircleCollider2D>();
+        Assert.IsNotNull(collider, "Player must have a " + collider.GetType().ToString());
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
     {
         if (other.collider.CompareTag(GameConstants.TAG_POLLEN))
         {
             other.transform.GetComponent<Rigidbody2D>().isKinematic = true;
             other.transform.SetParent(transform);
             other.transform.localPosition = Vector2.up * -0.8f;
-            other.collider.isTrigger = true;
+            other.collider.enabled = false;
             pollenCollider = other.collider;
         }
         else if (pollenCollider != null && other.collider.CompareTag(GameConstants.TAG_PLAYER))
         {
-            //pollenCollider.isTrigger = false;
+            //pollenCollider.enabled = true;
             pollenCollider.transform.SetParent(null);
             pollenCollider.GetComponent<Rigidbody2D>().isKinematic = false;
             pollenCollider = null;
@@ -29,6 +44,14 @@ public class PollenCollector : MonoBehaviour
             {
                 onPollenDrop.Invoke();
             }
+        }
+    }
+
+    public void RemovePollen()
+    {
+        if(pollenCollider != null)
+        {
+            Destroy(pollenCollider.gameObject);
         }
     }
 }
